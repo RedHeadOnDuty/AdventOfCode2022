3142b01612ad4fd0e510b2a1f7e3c406abf3af6f
RedHeadOnDuty <puddingwackel06@gmail.com>
2022-12-06 20:39:47 +0100

    Day3 Finish

--- Day3/Day3.cpp
+++ Day3/Day3.cpp
@@ -2,11 +2,16 @@
 // Created by Casual on 04.12.2022.
 //
 
+#include <set>
 #include "../HelperClass.h"
 
-inline void Day3()
+inline int calculate_priority(char item){
+    return ((int)(item / 'a')? item - 'a' + 1 : item - 'A' + 26 + 1);
+}
+
+inline int Day3_1()
 {
-    auto file = HelperClass::get_basic_ifstream(R"(Day3\SourceDay3.txt)");
+    std::ifstream file (R"(..\Day3\SourceDay3.txt)");
     int priority_total = 0;
 
     std::string line;
@@ -15,35 +20,66 @@ inline void Day3()
         int half_line_size = (int)line.size() / 2;
         char line_half_1 [half_line_size];
         char line_half_2 [half_line_size];
+
         line.copy(line_half_1, half_line_size, 0);
         line.copy(line_half_2, half_line_size, half_line_size);
 
-        for (int i = 0; i < half_line_size; ++i) {
-            if(line_half_1[i] - line_half_2[i])
-                for (int j = 0; j < ; ++j) {
-                    
-                }
-                continue;
-            else
+        std::set<char> line_half_1_set(line_half_1, line_half_1 + sizeof(line_half_1));
+        std::set<char> line_half_2_set(line_half_2, line_half_2 + sizeof(line_half_2));
+
+        for (auto set_1_item : line_half_1_set)
+        {
+            for (auto set_2_item: line_half_2_set)
             {
-                int priority = (int)(line_half_2[i] / 'a')? line_half_2[i] - 'a' + 1 : line_half_2[i] - 'A' + 26;
-                priority_total += priority;
-                std::cout << line_half_2[i] << " -> " << priority;
-                std::cout << "\t" << (int)(line_half_2[i] / 'a') << std::endl;
-                break;
+                if(set_1_item == set_2_item)
+                {
+                    priority_total += calculate_priority(set_2_item);
+                    std::cout << set_2_item << " -> " << calculate_priority(set_2_item) << std::endl;
+                }
             }
         }
-
-
         std::printf("\nhalf line size = %d, line.size() = %d \n", half_line_size, (int)line.size());
         std::printf("Original: %s\n", line.c_str());
         line_half_1[half_line_size] = '\0';
         line_half_2[half_line_size] = '\0';
         std::printf("first half(%d): %s \t second half(%d): %s\n",
                     (int)sizeof(line_half_2), line_half_1, (int)sizeof line_half_2, line_half_2);
-        
-
     }
-    std::cout << priority_total << std::endl;
+    return priority_total;
+}
+
+
+inline int Day3_2()
+{
+    std::ifstream file (R"(..\Day3\SourceDay3.txt)");
+    int priority_total = 0;
+    std::string line_array[3];
+    while (
+        std::getline(file, line_array[0]) &&
+        std::getline(file, line_array[1]) &&
+        std::getline(file, line_array[2])   )
+    {
+
+        std::set<char> line_1_set(std::begin(line_array[0]), std::end(line_array[0]));
+        std::set<char> line_2_set(std::begin(line_array[1]), std::end(line_array[1]));
+        std::set<char> line_3_set(std::begin(line_array[2]), std::end(line_array[2]));
+
+        for (auto set_1_item : line_1_set)
+            for (auto set_2_item: line_2_set)
+                if(set_1_item == set_2_item)
+                    for (auto set_3_item: line_3_set)
+                        if(set_1_item == set_3_item)
+                        {
+                            priority_total += calculate_priority(set_2_item);
+                            std::cout << set_2_item << " -> " << calculate_priority(set_2_item) << std::endl;
+                            std::cout << set_2_item << " -> " << calculate_priority(set_2_item) << std::endl;
+                        }
+    }
+    return priority_total;
+}
+
+inline void Day3(){
+    std::cout << "Priority 1 Total: " << Day3_1() << std::endl;
+    std::cout << "Priority 2 Total: " << Day3_2() << std::endl;
     std::cout << "Day3 Finished!" << std::endl;
 }
